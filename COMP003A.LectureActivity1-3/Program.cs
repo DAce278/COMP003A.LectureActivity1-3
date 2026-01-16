e4a8e2051b8882f1ca675237ce96e03178339a21
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-01-15 21:01:15 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1-3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
